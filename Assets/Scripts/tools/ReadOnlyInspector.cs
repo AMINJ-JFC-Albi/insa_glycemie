a820808b316e2557f74202334c5bb4e8b11d5eab
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2025-01-05 16:14:51 +0100

    dev/ui : Implement Placement Interactions System, Implement Exploded View Interactions & Tests

---
+++ Assets/Scripts/tools/ReadOnlyInspector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ReadOnlyAttribute : PropertyAttribute { }
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+
+        EditorGUI.PropertyField(position, property, label, true);
+
+        GUI.enabled = true;
+    }
+}
---
+++ Assets/Scripts/tools/ResetChildrenTransforms.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetChildrenTransforms : MonoBehaviour
+{
+    private class TransformData
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public Vector3 Scale;
+
+        public TransformData(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
+    }
+
+    private Dictionary<Transform, TransformData> initialTransforms = new Dictionary<Transform, TransformData>();
+
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            initialTransforms[child] = new TransformData(child.localPosition, child.localRotation, child.localScale);
+        }
+    }
+
+    public void ResetObjects()
+    {
+        foreach (var entry in initialTransforms)
+        {
+            if (entry.Key != null && entry.Key.gameObject.activeInHierarchy)
+            {
+                entry.Key.SetLocalPositionAndRotation(entry.Value.Position, entry.Value.Rotation);
+                entry.Key.localScale = entry.Value.Scale;
+                if (entry.Key.TryGetComponent<Rigidbody>(out var rb))
+                {
+                    rb.linearVelocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                }
+            }
+        }
+    }
+}
