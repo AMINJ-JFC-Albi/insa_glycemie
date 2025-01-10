46c27dbc1733afbf7fa442d9a4fe2bab18d34537
Florian <florianlloret13012@gmail.com>
2025-01-10 14:57:46 +0100

    Add TTS for room 1
    Need to implement the official TTS file
    Need to implement an Assistant

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -31,6 +32,7 @@ public class DialogueSystemEditor : Editor
 
 public class DialogueSystem : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] listTTS;
     [SerializeField] private TextMeshProUGUI dialogueTMP;
     [SerializeField] private List<string> texts = new()
     {
@@ -40,10 +42,13 @@ public class DialogueSystem : MonoBehaviour
         "Excellent ! Maintenant, dépose l'objet sur la table de craft pour commencer l'assemblage.",
         "Il te reste encore [X] composants à récupérer dans la salle. Suis les lignes lumineuses pour les retrouver."
     };
+
+    private AudioSource audioSource;
     private int progressionStep = 0;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         if (dialogueTMP == null) throw new System.Exception("TextMeshProUGUI dialogueTMP null !");
         if (texts.Count == 0) throw new System.Exception("List<string> texts is empty !");
         ShowDialogue();
@@ -58,6 +63,7 @@ public class DialogueSystem : MonoBehaviour
     {
         progressionStep++;
         if (progressionStep >= texts.Count) progressionStep = texts.Count;
+        if (progressionStep >= listTTS.Length) progressionStep = listTTS.Length;
     }
 
     public void ShowDialogue()
@@ -66,6 +72,10 @@ public class DialogueSystem : MonoBehaviour
             dialogueTMP.text = "";
         else
             dialogueTMP.text = ParseText(texts[progressionStep]);
+
+        if (progressionStep < listTTS.Length) 
+            audioSource.clip = listTTS[progressionStep];
+            audioSource.Play();
     }
 
     public string ParseText(string text)
