b509adc7a52f8420951006e80f690d36208e1884
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-11-08 18:18:53 +0100

    RESTART of the project !

--- Assets/Scripts/Managers/VRPlacementInteractions/IPlacementAction.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public abstract class IPlacementAction : MonoBehaviour
-{
-    //Execute : fonction à arguments variadiques de type object (tout type de classe)
-    public abstract void Execute(params object[] args);
-}
