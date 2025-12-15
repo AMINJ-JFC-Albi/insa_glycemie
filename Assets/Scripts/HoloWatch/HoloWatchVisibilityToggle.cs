d857429e3ee56050181282bb0fbb0190338e60c7
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-15 10:17:45 +0100

    Add basic VRHand

---
+++ Assets/Scripts/HoloWatch/HoloWatchVisibilityToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace HoloWatch {
+    public class HoloWatchVisibilityToggle : MonoBehaviour{
+        [SerializeField] private InputActionReference controllerActionButton;
+        private Canvas canvas;
+        
+        private void Start() {
+            canvas = GetComponent<Canvas>();
+            canvas.enabled = false;
+        }
+        
+        private void OnEnable() {
+            controllerActionButton.action.performed += ToggleHoloWatch;
+        }
+
+        private void OnDisable() {
+            controllerActionButton.action.performed -= ToggleHoloWatch;
+        }
+
+        private void ToggleHoloWatch(InputAction.CallbackContext obj) {
+            canvas.enabled = !canvas.enabled;
+        }
+    }
+}
