75558504c4d39ab01a93b9912162cb5cdddb078f
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-15 10:17:45 +0100

    Add basic VRHand

---
+++ Assets/Scripts/VRHands/Finger.cs
@@ -0,0 +1,21 @@
+﻿namespace VrHands {
+    //Utilisé dans HandAnimator
+    public class Finger {
+        public readonly FingerType Type;
+        public float Current = 0.0f;
+        public float Target = 0.0f;
+
+        public Finger(FingerType type) {
+            Type = type;
+        }
+    }
+
+    public enum FingerType {
+        None,
+        Thumb,
+        Index,
+        Middle,
+        Ring,
+        Pinky
+    }
+}
