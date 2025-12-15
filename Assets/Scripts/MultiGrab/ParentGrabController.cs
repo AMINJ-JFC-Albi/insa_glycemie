cc780d266a51d0476c36986ab86d8d7dd33f57f9
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-15 17:28:18 +0100

    Add MultiGrab

---
+++ Assets/Scripts/MultiGrab/ParentGrabController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class ParentGrabController : MonoBehaviour {
+    private XRGrabInteractable grab;
+    private DetachableSubObject[] subObjects;
+
+    private void Awake() {
+        grab = GetComponent<XRGrabInteractable>();
+        subObjects = transform.parent.GetComponentsInChildren<DetachableSubObject>(true);
+
+        grab.selectEntered.AddListener(OnGrab);
+        grab.selectExited.AddListener(OnRelease);
+    }
+
+    private void Start() {
+        foreach (DetachableSubObject subObject in subObjects) {
+            subObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args) {
+        foreach (DetachableSubObject sub in subObjects) {
+            sub.EnableDetach(true);
+        }
+    }
+
+    private void OnRelease(SelectExitEventArgs args) {
+        foreach (DetachableSubObject sub in subObjects) {
+            sub.EnableDetach(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MultiGrab/SpecialObjectPart.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class SpecialObjectPart : MonoBehaviour {
+    [Header("Références")] public Rigidbody mainRigidbody; // rigidbody du SpecialObject
+    public XRGrabInteractable mainGrab; // XRGrabInteractable du parent
+
+    [Header("Prefab de partie détachée")] public GameObject detachedPrefab; // optionnel : prefab version indépendante
+    public bool IsDetached { get; private set; }
+
+    private Collider[] _colliders;
+
+    private void Awake() {
+        _colliders = GetComponentsInChildren<Collider>();
+    }
+
+    public bool ContainsCollider(Collider col) {
+        if (col == null || _colliders == null)
+            return false;
+
+        for (int i = 0; i < _colliders.Length; i++) {
+            if (_colliders[i] == col)
+                return true;
+        }
+        return false;
+    }
+
+    public void DetachFromParent() {
+        if (IsDetached)
+            return;
+        IsDetached = true;
+
+        if (detachedPrefab != null) {
+            // version "propre" : on instancie une nouvelle version grabbable indépendante
+            var instance = Instantiate(detachedPrefab, transform.position, transform.rotation);
+            // tu peux ajouter XRGrabInteractable + Rigidbody sur le prefab
+        }
+
+        // soit on rend cette partie indépendante directement
+        transform.SetParent(null);
+
+        // on peut ajouter un Rigidbody si absent
+        var rb = GetComponent<Rigidbody>();
+        if (rb == null)
+            rb = gameObject.AddComponent<Rigidbody>();
+
+        // on peut aussi ajouter un XRGrabInteractable pour cette partie seule
+        var grab = GetComponent<XRGrabInteractable>();
+        if (grab == null) {
+            grab = gameObject.AddComponent<XRGrabInteractable>();
+            grab.movementType = XRBaseInteractable.MovementType.VelocityTracking;
+            grab.throwOnDetach = true;
+        }
+    }
+}
