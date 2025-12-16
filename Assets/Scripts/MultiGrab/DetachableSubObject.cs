98f7391d4c13c5bc6675016779b9fcde438ca157
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-16 14:59:42 +0100

    Add Head Collider

--- Assets/Scripts/MultiGrab/DetachableSubObject.cs
+++ Assets/Scripts/MultiGrab/DetachableSubObject.cs
@@ -3,84 +3,86 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class DetachableSubObject : MonoBehaviour {
-    private XRGrabInteractable grab;
-    private Rigidbody rb;
-    private Renderer rend;
-    private Color[] originalColors;
+namespace MultiGrab {
+    public class DetachableSubObject : MonoBehaviour {
+        private XRGrabInteractable grab;
+        private Rigidbody rb;
+        private Renderer rend;
+        private Color[] originalColors;
 
-    public DetachableSubObject prerequisiteObject;
-    private bool isDetached = false;
+        public DetachableSubObject prerequisiteObject;
+        private bool isDetached = false;
 
-    private void Awake() {
-        grab = GetComponent<XRGrabInteractable>();
-        rb = GetComponent<Rigidbody>();
-        rend = GetComponent<Renderer>();
-        if (rend) {
-            originalColors = new Color[rend.materials.Length];
-            for (int i = 0; i < rend.materials.Length; i++) {
-                originalColors[i] = rend.materials[i].color;
+        private void Awake() {
+            grab = GetComponent<XRGrabInteractable>();
+            rb = GetComponent<Rigidbody>();
+            rend = GetComponent<Renderer>();
+            if (rend) {
+                originalColors = new Color[rend.materials.Length];
+                for (int i = 0; i < rend.materials.Length; i++) {
+                    originalColors[i] = rend.materials[i].color;
+                }
+            }
+
+            grab.enabled = false;
+            rb.isKinematic = true;
+
+            grab.selectEntered.AddListener(OnGrabbed);
+            grab.hoverEntered.AddListener(OnHoverEnter);
+            grab.hoverExited.AddListener(OnHoverExit);
+        }
+
+        public void EnableDetach(bool enabled) {
+            if (isDetached || (prerequisiteObject != null && !prerequisiteObject.isDetached)) {
+                return;
+            }
+            grab.enabled = enabled;
+        }
+
+        private void OnGrabbed(SelectEnterEventArgs args) {
+            if (isDetached) {
+                return;
+            }
+            isDetached = true;
+
+            // Force XR à lâcher toute relation précédente
+            XRInteractionManager interactionManager = grab.interactionManager;
+            IXRSelectInteractor interactor = args.interactorObject;
+            interactionManager.SelectExit(interactor, grab);
+
+            rb.isKinematic = false;
+            transform.SetParent(null);
+            grab.enabled = true;
+            gameObject.layer = LayerMask.NameToLayer("Default");
+
+            if (rend) {
+                for (int i = 0; i < rend.materials.Length; i++) {
+                    rend.materials[i].color = originalColors[i];
+                }
+            }
+
+            interactionManager.SelectEnter(interactor, grab);
+        }
+
+        private void OnHoverEnter(HoverEnterEventArgs args) {
+            if (!isDetached && rend && (prerequisiteObject == null || prerequisiteObject.isDetached)) {
+                // Rendre chaque matériau un peu bleuté et transparent
+                for (int i = 0; i < rend.materials.Length; i++) {
+                    Color c = originalColors[i];
+                    c.r = 0.5f;
+                    c.g = 0.7f;
+                    c.b = 0.7f;
+                    rend.materials[i].color = c;
+                }
             }
         }
 
-        grab.enabled = false;
-        rb.isKinematic = true;
-
-        grab.selectEntered.AddListener(OnGrabbed);
-        grab.hoverEntered.AddListener(OnHoverEnter);
-        grab.hoverExited.AddListener(OnHoverExit);
-    }
-
-    public void EnableDetach(bool enabled) {
-        if (isDetached || (prerequisiteObject != null && !prerequisiteObject.isDetached)) {
-            return;
-        }
-        grab.enabled = enabled;
-    }
-
-    private void OnGrabbed(SelectEnterEventArgs args) {
-        if (isDetached) {
-            return;
-        }
-        isDetached = true;
-
-        // Force XR à lâcher toute relation précédente
-        XRInteractionManager interactionManager = grab.interactionManager;
-        IXRSelectInteractor interactor = args.interactorObject;
-        interactionManager.SelectExit(interactor, grab);
-
-        rb.isKinematic = false;
-        transform.SetParent(null);
-        grab.enabled = true;
-        gameObject.layer = LayerMask.NameToLayer("Default");
-        
-        if (rend) {
-            for (int i = 0; i < rend.materials.Length; i++) {
-                rend.materials[i].color = originalColors[i];
-            }
-        }
-
-        interactionManager.SelectEnter(interactor, grab);
-    }
-
-    private void OnHoverEnter(HoverEnterEventArgs args) {
-        if (!isDetached && rend && (prerequisiteObject == null || prerequisiteObject.isDetached)) {
-            // Rendre chaque matériau un peu bleuté et transparent
-            for (int i = 0; i < rend.materials.Length; i++) {
-                Color c = originalColors[i];
-                c.r = 0.5f;
-                c.g = 0.7f;
-                c.b = 0.7f;
-                rend.materials[i].color = c;
-            }
-        }
-    }
-
-    private void OnHoverExit(HoverExitEventArgs args) {
-        if (!isDetached && rend && (prerequisiteObject == null || prerequisiteObject.isDetached)) {
-            // Restaurer les couleurs originales
-            for (int i = 0; i < rend.materials.Length; i++) {
-                rend.materials[i].color = originalColors[i];
+        private void OnHoverExit(HoverExitEventArgs args) {
+            if (!isDetached && rend && (prerequisiteObject == null || prerequisiteObject.isDetached)) {
+                // Restaurer les couleurs originales
+                for (int i = 0; i < rend.materials.Length; i++) {
+                    rend.materials[i].color = originalColors[i];
+                }
             }
         }
     }
--- Assets/Scripts/MultiGrab/ParentGrabController.cs
+++ Assets/Scripts/MultiGrab/ParentGrabController.cs
@@ -2,33 +2,35 @@
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class ParentGrabController : MonoBehaviour {
-    private XRGrabInteractable grab;
-    private DetachableSubObject[] subObjects;
+namespace MultiGrab {
+    public class ParentGrabController : MonoBehaviour {
+        private XRGrabInteractable grab;
+        private DetachableSubObject[] subObjects;
 
-    private void Awake() {
-        grab = GetComponent<XRGrabInteractable>();
-        subObjects = transform.parent.GetComponentsInChildren<DetachableSubObject>(true);
+        private void Awake() {
+            grab = GetComponent<XRGrabInteractable>();
+            subObjects = transform.parent.GetComponentsInChildren<DetachableSubObject>(true);
 
-        grab.selectEntered.AddListener(OnGrab);
-        grab.selectExited.AddListener(OnRelease);
-    }
-
-    private void Start() {
-        foreach (DetachableSubObject subObject in subObjects) {
-            subObject.transform.SetParent(transform);
+            grab.selectEntered.AddListener(OnGrab);
+            grab.selectExited.AddListener(OnRelease);
         }
-    }
 
-    private void OnGrab(SelectEnterEventArgs args) {
-        foreach (DetachableSubObject sub in subObjects) {
-            sub.EnableDetach(true);
+        private void Start() {
+            foreach (DetachableSubObject subObject in subObjects) {
+                subObject.transform.SetParent(transform);
+            }
         }
-    }
 
-    private void OnRelease(SelectExitEventArgs args) {
-        foreach (DetachableSubObject sub in subObjects) {
-            sub.EnableDetach(false);
+        private void OnGrab(SelectEnterEventArgs args) {
+            foreach (DetachableSubObject sub in subObjects) {
+                sub.EnableDetach(true);
+            }
+        }
+
+        private void OnRelease(SelectExitEventArgs args) {
+            foreach (DetachableSubObject sub in subObjects) {
+                sub.EnableDetach(false);
+            }
         }
     }
 }
