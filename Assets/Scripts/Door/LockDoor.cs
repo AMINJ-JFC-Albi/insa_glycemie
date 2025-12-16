c84305131530d85b1597928520a66b205ece0c60
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-16 14:59:42 +0100

    Add Head Collider

---
+++ Assets/Scripts/Door/LockDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+namespace Door {
+    public class LockDoor : MonoBehaviour {
+        private XRGrabInteractable grab;
+        private bool isLocked = true;
+        
+        private void Awake() {
+            grab = GetComponent<XRGrabInteractable>();
+            grab.enabled = false;
+        }
+        
+        public void UnlockDoor() {
+            isLocked = false;
+            grab.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/VRBody/HeadCollisionHandler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class HeadCollisionHandler : MonoBehaviour {
+    [SerializeField] private HeadCollisionDetector detector;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] public float pushBackStrength = 1.0f;
+    
+    private Vector3 CalculatePushBackDirection(List<RaycastHit> colliderHits) {
+        Vector3 combinedNormal = Vector3.zero;
+        foreach (RaycastHit hitPoint in colliderHits) {
+            combinedNormal += new Vector3(hitPoint.normal.x, 0, hitPoint.normal.z);
+        }
+        return combinedNormal;
+    }
+
+    private void Update() {
+        if (detector.detectedColliderHits.Count <= 0) {
+            return;
+        }
+        Vector3 pushBackDirection = CalculatePushBackDirection(detector.detectedColliderHits);
+
+        Debug.DrawRay(transform.position, pushBackDirection.normalized, Color.magenta);
+
+        characterController.Move(pushBackDirection.normalized * (pushBackStrength * Time.deltaTime));
+    }
+}
