d7c9f06ac3bea1ddae7613519a24dcb90479d322
Onilaf <60063118+Onilaf@users.noreply.github.com>
2025-12-19 00:45:17 +0100

    Add HoloWatch Graphic

---
+++ Assets/Scripts/HoloWatch/ObjectiveManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace HoloWatch {
+    public class ObjectiveManager : MonoBehaviour {
+        public static ObjectiveManager Instance;
+
+        private Dictionary<string, ObjectiveData> activeObjectives;
+        private HashSet<string> completedObjectives;
+
+        private void Awake() {
+            if (Instance != null && Instance != this) {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+
+            activeObjectives = new Dictionary<string, ObjectiveData>();
+            completedObjectives = new HashSet<string>();
+        }
+
+        public void AddObjective(ObjectiveData objective) {
+            if (activeObjectives.ContainsKey(objective.objectiveId)) {
+                return;
+            }
+
+            activeObjectives.Add(objective.objectiveId, objective);
+            HolowatchUI.Instance.RefreshObjectives();
+        }
+
+        public void CompleteObjective(string objectiveId) {
+            if (!activeObjectives.ContainsKey(objectiveId)) {
+                return;
+            }
+
+            completedObjectives.Add(objectiveId);
+            activeObjectives.Remove(objectiveId);
+
+            HolowatchUI.Instance.RefreshObjectives();
+            HolowatchUI.Instance.NotifyDoc("Objectif validé");
+        }
+
+        public IEnumerable<ObjectiveData> GetActiveObjectives() {
+            return activeObjectives.Values;
+        }
+    }
+}
---
+++ Assets/Scripts/HoloWatch/StoryData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace HoloWatch {
+    [CreateAssetMenu(menuName = "Holowatch/Story", order = 0)]
+    public class StoryData : ScriptableObject {
+        public ObjectiveData mainObjective;
+        public List<ObjectiveData> objectives;
+    }
+}
