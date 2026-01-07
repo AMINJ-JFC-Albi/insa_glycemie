ddd4317821ef35897396caa4f21219c385761cfe
Onilaf <60063118+Onilaf@users.noreply.github.com>
2026-01-07 16:47:49 +0100

    Fix Merge Couloir/Labo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using HoloWatch;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public static GameManager Instance;
+    
+    public StoryData storyData;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
+    public Transform holoWatch;
+    public Transform rightAnchorHoloWatch;
+    public Transform leftAnchorHoloWatch;
+    
+    private List<string> objIds;
+
+    private void Awake() {
+        if (Instance == null) {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
+        } else {
             Destroy(gameObject);
         }
     }
+    
+    void Start() {
+        holoWatch.SetParent(rightAnchorHoloWatch);
+        objIds = new List<string> { storyData.mainObjective.objectiveId };
+        foreach (ObjectiveData obj in storyData.objectives) {
+            objIds.Add(obj.objectiveId);
+        }
+        HolowatchUI.Instance.InitStory(storyData);
+        StartCoroutine(DocMessage());
+    }
 
-    public void LoadScene(string sceneName)
-    {
+    private IEnumerator DocMessage() {
+        yield return new WaitForSeconds(5f);
+        HolowatchUI.Instance.AddMessage(objIds[0], "First");
+        yield return new WaitForSeconds(10f);
+        HolowatchUI.Instance.AddMessage(objIds[0], "PS");
+        HolowatchUI.Instance.SetNextHints(objIds[1], new List<string>() {"1-1"});
+    }
+    
+    public void LoadScene(string sceneName) {
         StartCoroutine(LoadSceneVR(sceneName));
     }
 
-    IEnumerator LoadSceneVR(string sceneName)
-    {
+    IEnumerator LoadSceneVR(string sceneName) {
         yield return new WaitForSeconds(0.5f);
 
         SceneManager.LoadScene(sceneName);
