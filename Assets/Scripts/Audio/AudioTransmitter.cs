a7e0114d5db5b9eb42015571720ca6f48d0f0b8a
Onilaf <60063118+Onilaf@users.noreply.github.com>
2026-01-07 15:04:20 +0100

    Add AudioSource + Subtitle

---
+++ Assets/Scripts/Audio/AudioTransmitter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Audio {
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioTransmitter : MonoBehaviour {
+        private AudioSource audioSource;
+        [SerializeField] private AudioClip audioClip;
+        
+        private void Awake() {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Play() {
+            if (audioSource != null && audioClip != null) {
+                audioSource.PlayOneShot(audioClip);
+                StartCoroutine(GetComponent<Subtitles>().Play());
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/Subtitles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Audio {
+    public class Subtitles : MonoBehaviour {
+        private TMP_Text masterText;
+        private TMP_Text graphicText;
+
+        public IEnumerator Play() {
+            Text("Oui ? Fait voir... mmmh intéressant.");
+            yield return new WaitForSeconds(1f);
+            Text("Alors je le répète pour être clair : ce qui est mesuré dans le sang, c’est la glycémie capillaire,");
+            yield return new WaitForSeconds(1f);
+            Text("pas la glycémie interstitielle… la nuance change tout.");
+            yield return new WaitForSeconds(1f);
+            Text("*soupir* Si seulement ils avaient compris ça plus tôt...");
+            yield return new WaitForSeconds(1f);
+            Text("Bref. Passons.");
+        }
+        
+        private void Text(string text) {
+            masterText.text = text;
+            graphicText.text = text;
+        }
+    }
+}
