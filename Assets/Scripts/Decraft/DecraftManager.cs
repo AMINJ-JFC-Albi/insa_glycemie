7ab5980478d10ae201a2a5b547b4124180a809d9
Onilaf <60063118+Onilaf@users.noreply.github.com>
2026-01-09 00:40:51 +0100

    Fix DecraftTable

---
+++ Assets/Scripts/Decraft/DecraftManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Decraft {
+    public class DecraftManager : MonoBehaviour {
+        [SerializeField] private ConfigurableJoint targetJoint;
+        private int currentCount;
+        
+        public void Add() {
+            currentCount++;
+            if (currentCount >= 4) {
+                targetJoint.zMotion = ConfigurableJointMotion.Limited;
+            }
+        }
+        
+        public void Remove() {
+            currentCount--;
+        }
+    }
+}
---
+++ Assets/Scripts/Decraft/DecraftObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Decraft {
+    public enum DecraftType {
+        Base,
+        Aiguille,
+        Carte,
+        Cache
+    }
+    
+    public class DecraftObject : MonoBehaviour{
+        public DecraftType decraftType;
+    }
+}
