7654b07111c501ad41ba3365147b47cf7a3842dd
Oxy-More236987 <159430947+Oxy-More236987@users.noreply.github.com>
2026-01-11 16:40:46 +0100

    Fix probleme grabable red key

--- Assets/Scripts/Decraft/DecraftManager.cs
+++ Assets/Scripts/Decraft/DecraftManager.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 namespace Decraft {
     public class DecraftManager : MonoBehaviour {
         [SerializeField] private ConfigurableJoint targetJoint;
         private int currentCount;
+
+        public XRGrabInteractable key;
+
+        void Start()
+        {
+            key.enabled = false;
+        }
         
         public void Add() {
             currentCount++;
             if (currentCount >= 4) {
                 targetJoint.zMotion = ConfigurableJointMotion.Limited;
+                key.enabled = true;
             }
         }
         
